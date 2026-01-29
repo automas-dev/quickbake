830b73773fc05c6f160acfc612a2812a879367d1
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-01-29 02:01:35 -0500

    Is it manifest or bl_info?

--- src/quickbake/__init__.py
+++ src/quickbake/__init__.py
@@ -1,18 +1,17 @@
-import importlib.metadata
-
 import bpy
 
-# from .op import QuickBake_OT_bake
-# from .panel import QuickBake_PT_main
-# from .properties import QuickBakeToolPropertyGroup
+from .op import QuickBake_OT_bake
+from .panel import QuickBake_PT_main
+from .properties import QuickBakeToolPropertyGroup
 
-version_str = importlib.metadata.version('quickbake').strip('v')
+# the value DEV_BUILD is replaced with the version string by ci
+version_str = '0.0.1'.strip('v').split('-', 1)[0]
 version_tuple = tuple(map(int, version_str.split('.')))
 
 bl_info = {
     'name': 'Quick Bake',
     'author': 'Thomas Harrison',
-    'description': '',
+    'description': 'Fast baking for blender',
     'blender': (2, 80, 0),
     'version': version_tuple,
     'location': '',
@@ -21,21 +20,21 @@ bl_info = {
 }
 
 
-# def register():
-#     bpy.utils.register_class(QuickBake_OT_bake)
-#     bpy.utils.register_class(QuickBake_PT_main)
-#     bpy.utils.register_class(QuickBakeToolPropertyGroup)
-#     bpy.types.Scene.QuickBakeToolPropertyGroup = bpy.props.PointerProperty(
-#         type=QuickBakeToolPropertyGroup
-#     )
+def register():
+    bpy.utils.register_class(QuickBake_OT_bake)
+    bpy.utils.register_class(QuickBake_PT_main)
+    bpy.utils.register_class(QuickBakeToolPropertyGroup)
+    bpy.types.Scene.QuickBakeToolPropertyGroup = bpy.props.PointerProperty(
+        type=QuickBakeToolPropertyGroup
+    )
 
 
-# def unregister():
-#     bpy.utils.unregister_class(QuickBake_OT_bake)
-#     bpy.utils.unregister_class(QuickBake_PT_main)
-#     bpy.utils.unregister_class(QuickBakeToolPropertyGroup)
-#     del bpy.types.Scene.QuickBakeToolPropertyGroup
+def unregister():
+    bpy.utils.unregister_class(QuickBake_OT_bake)
+    bpy.utils.unregister_class(QuickBake_PT_main)
+    bpy.utils.unregister_class(QuickBakeToolPropertyGroup)
+    del bpy.types.Scene.QuickBakeToolPropertyGroup
 
 
-# if __name__ == '__main__':
-#     register()
+if __name__ == '__main__':
+    register()
