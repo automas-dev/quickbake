87d9356a4b52e8c8f00537ab7e4257dba9fe0059
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-01-29 02:09:59 -0500

    Back to prod state again

--- src/quickbake/__init__.py
+++ src/quickbake/__init__.py
@@ -4,21 +4,6 @@ from .op import QuickBake_OT_bake
 from .panel import QuickBake_PT_main
 from .properties import QuickBakeToolPropertyGroup
 
-# the value DEV_BUILD is replaced with the version string by ci
-version_str = '0.0.1'.strip('v').split('-', 1)[0]
-version_tuple = tuple(map(int, version_str.split('.')))
-
-bl_info = {
-    'name': 'Quick Bake',
-    'author': 'Thomas Harrison',
-    'description': 'Fast baking for blender',
-    'blender': (2, 80, 0),
-    'version': version_tuple,
-    'location': '',
-    'warning': '',
-    'category': 'Render',
-}
-
 
 def register():
     bpy.utils.register_class(QuickBake_OT_bake)
