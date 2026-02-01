6522a1a2a0d4abf13e945906ec91c0d735f45215
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-02-01 18:17:16 -0500

    Format (#4)
    * Use 0.0.0 as default version, update ci to match version string
    * Remove uv from build release
    * Add targets to makefile for build, install and run
    * Add ruff format to ci lint
    * Format code with ruff format

--- Makefile
+++ Makefile
@@ -1,16 +1,32 @@
 
+INSTALL_DIR=$(HOME)/.config/blender/5.0/extensions/user_default/quickbake
+ZIP_ARCHIVE_NAME=quickbake_0.0.0.zip
+
 setup:
 	uv sync
+
+build_dev:
+	@mkdir -p release/
+	@cd src/quickbake && zip $(PWD)/release/$(ZIP_ARCHIVE_NAME) *
+
+install:
+	rm -rf $(INSTALL_DIR)/*
+	cp -r src/quickbake/* $(INSTALL_DIR)/
+
+run: install
+	blender
 
 checks: lint test
 
 lint:
+	uv run ruff format --check
 	uv run ruff check .
 
 format:
+	uv run ruff format
 	uv run ruff check --fix .
 
 test:
 	uv run pytest
 
-.PHONY: setup install checks lint format test
+.PHONY: setup build_dev install checks lint format test
--- src/quickbake/properties.py
+++ src/quickbake/properties.py
@@ -1,126 +1,101 @@
+# pyright: reportInvalidTypeForm=false
 import bpy
 
 
 class QuickBakeToolPropertyGroup(bpy.types.PropertyGroup):
-    reuse_tex: bpy.props.BoolProperty(  # type: ignore
-        name="Re-use Texture",
-        description="Use the texture from previous bakes",
-        default=True
+    reuse_tex: bpy.props.BoolProperty(
+        name='Re-use Texture',
+        description='Use the texture from previous bakes',
+        default=True,
     )
 
-    clean_up: bpy.props.BoolProperty(  # type: ignore
-        name="Clean Up",
-        description="Remove generated nodes after baking",
-        default=True
+    clean_up: bpy.props.BoolProperty(
+        name='Clean Up', description='Remove generated nodes after baking', default=True
     )
 
-    create_mat: bpy.props.BoolProperty(  # type: ignore
-        name="Create Material",
-        description="Create a material after baking and assign it.",
-        default=True
+    create_mat: bpy.props.BoolProperty(
+        name='Create Material',
+        description='Create a material after baking and assign it.',
+        default=True,
     )
 
-    mat_name: bpy.props.StringProperty(  # type: ignore
-        name="Name",
-        description="Name used to create a new material after baking",
-        default="BakeMaterial"
+    mat_name: bpy.props.StringProperty(
+        name='Name',
+        description='Name used to create a new material after baking',
+        default='BakeMaterial',
     )
 
-    save_img: bpy.props.BoolProperty(  # type: ignore
-        name="Save Images",
-        description="Write images to file after baking",
-        default=False
+    save_img: bpy.props.BoolProperty(
+        name='Save Images',
+        description='Write images to file after baking',
+        default=False,
     )
 
-    image_path: bpy.props.StringProperty(  # type: ignore
-        name="Texture Path",
-        description="Directory for baking output",
+    image_path: bpy.props.StringProperty(
+        name='Texture Path',
+        description='Directory for baking output',
         default='',
-        subtype='DIR_PATH'
+        subtype='DIR_PATH',
     )
 
-    bake_name: bpy.props.StringProperty(  # type: ignore
-        name="Name",
-        description="Name used fot the baked texture images",
-        default="BakeTexture"
+    bake_name: bpy.props.StringProperty(
+        name='Name',
+        description='Name used fot the baked texture images',
+        default='BakeTexture',
     )
 
-    bake_uv: bpy.props.StringProperty(  # type: ignore
-        name="UV",
-        description="Name used fot the uv bake layer",
-        default="bake_uv"
+    bake_uv: bpy.props.StringProperty(
+        name='UV', description='Name used fot the uv bake layer', default='bake_uv'
     )
 
-    bake_size: bpy.props.IntProperty(  # type: ignore
-        name="Size",
-        description="Resolution for the bake texture",
+    bake_size: bpy.props.IntProperty(
+        name='Size',
+        description='Resolution for the bake texture',
         default=1024,
         soft_min=1024,
-        step=1024
+        step=1024,
     )
 
-    diffuse_enabled: bpy.props.BoolProperty(  # type: ignore
-        name="Diffuse",
-        description="Bake the diffuse map",
-        default=True
+    diffuse_enabled: bpy.props.BoolProperty(
+        name='Diffuse', description='Bake the diffuse map', default=True
     )
 
-    normal_enabled: bpy.props.BoolProperty(  # type: ignore
-        name="Normal",
-        description="Bake the normal map",
-        default=True
+    normal_enabled: bpy.props.BoolProperty(
+        name='Normal', description='Bake the normal map', default=True
     )
 
-    roughness_enabled: bpy.props.BoolProperty(  # type: ignore
-        name="Roughness",
-        description="Bake the roughness map",
-        default=True
+    roughness_enabled: bpy.props.BoolProperty(
+        name='Roughness', description='Bake the roughness map', default=True
     )
 
-    ao_enabled: bpy.props.BoolProperty(  # type: ignore
-        name="Ao",
-        description="Bake the Ao map",
-        default=False
+    ao_enabled: bpy.props.BoolProperty(
+        name='Ao', description='Bake the Ao map', default=False
     )
 
-    shadow_enabled: bpy.props.BoolProperty(  # type: ignore
-        name="Shadow",
-        description="Bake the Shadow map",
-        default=False
+    shadow_enabled: bpy.props.BoolProperty(
+        name='Shadow', description='Bake the Shadow map', default=False
     )
 
-    position_enabled: bpy.props.BoolProperty(  # type: ignore
-        name="Position",
-        description="Bake the Position map",
-        default=False
+    position_enabled: bpy.props.BoolProperty(
+        name='Position', description='Bake the Position map', default=False
     )
 
-    uv_enabled: bpy.props.BoolProperty(  # type: ignore
-        name="Uv",
-        description="Bake the Uv map",
-        default=False
+    uv_enabled: bpy.props.BoolProperty(
+        name='Uv', description='Bake the Uv map', default=False
     )
 
-    emit_enabled: bpy.props.BoolProperty(  # type: ignore
-        name="Emit",
-        description="Bake the Emit map",
-        default=False
+    emit_enabled: bpy.props.BoolProperty(
+        name='Emit', description='Bake the Emit map', default=False
     )
 
-    environment_enabled: bpy.props.BoolProperty(  # type: ignore
-        name="Environment",
-        description="Bake the Environment map",
-        default=False
+    environment_enabled: bpy.props.BoolProperty(
+        name='Environment', description='Bake the Environment map', default=False
     )
 
-    glossy_enabled: bpy.props.BoolProperty(  # type: ignore
-        name="Glossy",
-        description="Bake the Glossy map",
-        default=False
+    glossy_enabled: bpy.props.BoolProperty(
+        name='Glossy', description='Bake the Glossy map', default=False
     )
 
-    transmission_enabled: bpy.props.BoolProperty(  # type: ignore
-        name="Transmission",
-        description="Bake the Transmission map",
-        default=False
+    transmission_enabled: bpy.props.BoolProperty(
+        name='Transmission', description='Bake the Transmission map', default=False
     )
