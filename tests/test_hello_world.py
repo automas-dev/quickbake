850443b9f5f13ca1ec8a67d296856e350c663f19
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-02-01 18:22:14 -0500

    Move quickbake out of src

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -4,15 +4,3 @@ from unittest.mock import patch
 
 def test_import():
     import quickbake
-
-
-# @patch('quickbake.bpy')
-def test_version():
-    import quickbake
-
-    with open('pyproject.toml', 'rb') as f:
-        t = tomllib.load(f)
-
-    expect_version = tuple(map(int, t['project']['version'].split('.')))
-
-    assert quickbake.version_tuple == expect_version
